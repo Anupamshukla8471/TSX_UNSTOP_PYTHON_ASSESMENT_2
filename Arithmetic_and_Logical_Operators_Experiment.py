7bcf262c4c398226b8aa36c0f1a32ef984ea67ce
anupamshukla8471 <anupamshukla8471@gmail.com>
2025-05-19 22:29:59 +0530

    # hello.py

---
+++ Arithmetic_and_Logical_Operators_Experiment.py
@@ -0,0 +1,16 @@
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Power:", a ** b)
+
+
+x = True
+y = False
+
+print("AND:", x and y)
+print("OR:", x or y)
+print("NOT X:", not x)
